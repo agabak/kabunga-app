9b6d7a3f5758cbf010f1066226ab40efb9cbe64b
Agaba Kabunga <Agaba Kabunga>
2017-10-01 10:43:15 -0500

    change the route

--- Startup.cs
+++ Startup.cs
@@ -8,15 +8,13 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Routing;
 
 namespace kabunga_app
 {
     public class Startup
     {
         private readonly IEventService _EventService;
-        public Startup(IHostingEnvironment env, IEventService eventService)
+        public Startup(IHostingEnvironment env,IEventService eventService)
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
@@ -35,7 +33,7 @@ namespace kabunga_app
             // Add framework services.
             services.AddMvc();
             services.AddRouting();
-
+            
             //Add  dependency injection 
             services.AddScoped<IImageService, ImageService>();
             services.AddScoped<IEventService, EventService>();
@@ -47,32 +45,9 @@ namespace kabunga_app
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
-            app.UseMvc();
-
-            var trackPackageRouteHandler = new RouteHandler(context =>
-                {
-                    var routeValues = context.GetRouteData().Values;
-                    return context.Response.WriteAsync(
-                        $"Hello! Route values: {string.Join(", ", routeValues)}");
-                });
-
-            var routeBuilder = new RouteBuilder(app, trackPackageRouteHandler);
-
-            routeBuilder.MapRoute(
-                "Track Package Route",
-                "package/{operation:regex(^(track|create|detonate)$)}/{id:int}");
-
-            routeBuilder.MapGet("hello/{name}", context =>
-            {
-                var name = context.GetRouteValue("name");
-                // This is the route handler when HTTP GET "hello/<anything>"  matches
-                // To match HTTP GET "hello/<anything>/<anything>,
-                // use routeBuilder.MapGet("hello/{*name}"
-                return context.Response.WriteAsync(this._EventService.GetEventByName("name").ToString());
+            app.UseMvc(routes => {
+                routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
-
-            var routes = routeBuilder.Build();
-            app.UseRouter(routes);
         }
     }
 }
